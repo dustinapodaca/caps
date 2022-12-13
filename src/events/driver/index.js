d7477f9f424a43eb43e5e94234eeeace08c84a6d
dustinapodaca <dustinapodaca@gmail.com>
2022-12-13 14:03:49 -0700

    working socket refactor

--- src/events/driver/index.js
+++ src/events/driver/index.js
@@ -2,17 +2,25 @@
 
 const eventPool = require('../../../EventEmitter');
 
-eventPool.on('PICKUP', pickedUp);
+require('dotenv').config();
+const { io } = require('socket.io-client');
+// const PORT = process.env.PORT || 3002;
+const socket = io(`http://localhost:3001/caps`);
+
+// eventPool.on('PICKUP', pickedUp);
+socket.on('pickup', pickedUp);
 
 function pickedUp(payload) {
   setTimeout(() => {
     console.log('DRIVER: picked up', payload.orderID);
     eventPool.emit('IN-TRANSIT', payload);
+    socket.emit('in-transit', payload);
   }, 1000);
 
   setTimeout(() => {
     console.log('DRIVER: delivered', payload.orderID);
     eventPool.emit('DELIVERED', payload);
+    socket.emit('delivered', payload);
   }, 2000);
 }
 
